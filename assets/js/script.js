c5ef3aaa5e7504df4a65ee3641c8f0173e5af231
azimezn <117550865+azimezn@users.noreply.github.com>
2022-12-06 23:38:24 -0500

    can fetch specific data for one day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
 var $submitButton = document.querySelector("#submit-button");
-var $cityInput = document.querySelector("#city-input")
-var $cityName = document.querySelector(".city-name")
+var $cityInput = document.querySelector("#city-input");
+var $cityName = document.querySelector(".city-name");
+var $currentDate = document.querySelector(".current-date");
+var $currentInfo = document.querySelector(".current-info");
 
 $submitButton.addEventListener('click', getCoordinates);
 
 function getCoordinates () {
-    var city = $cityInput.value
+    var city = $cityInput.value;
+    $cityName.textContent = city;
 
     var cityWeatherApi = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=48e33596e5a1232d3d02e65e8291a16d`
     
     fetch(cityWeatherApi).then(response=> {
 
         if(response.ok) response.json().then(data=>{
-            
+
             if (data.length === 0) {
                 console.log("no link");
             } else {
@@ -32,13 +35,16 @@ function getCoordinates () {
 
 function weatherInfo({lat, lon}) {
 
+    // "units=imperial" is to get results in fahrenheit
     var currentWeatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=48e33596e5a1232d3d02e65e8291a16d`;
+
   
     
     fetch(currentWeatherApi).then(response=> {
         console.log(response);
         if(response.ok) response.json().then(data=> {
             console.log(data);
+            displayWeather(data);
         })
 
 
@@ -46,13 +52,21 @@ function weatherInfo({lat, lon}) {
 
 }
 
-function displayWeather () {
-    data.main.temp
+function displayWeather (data) {
+
+    $currentInfo.innerHTML =
+    `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png"></img>
+    <li>Temperature: ${data.main.temp}</li>
+    <li>Humidity: ${data.main.humidity}</li>
+    <li>Wind Speed: ${data.wind.speed}</li>`;
+
+    
+
 }
 
 
-function displayForcast ()
+// function displayForcast ()
 
-function saveToLS ()
+// function saveToLS ()
 
-function createPrevCityClick ()
+// function createPrevCityClick ()
