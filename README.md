47f258830426a329ece38a4cf49b9c1889d01849
azimezn <117550865+azimezn@users.noreply.github.com>
2022-12-08 12:13:36 -0500

    added readme

--- README.md
+++ README.md
@@ -1,11 +1,33 @@
 # weather-dashboard
 
+## Description
 
-Your Task
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+You can get a 5-day weather forecast by typing in the city name. It will show the weather, temperature, wind speed, and humidity.
 
-Use the 5 Day Weather Forecast to retrieve weather data for cities. The base URL should look like the following: https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate.
+## Table of Contents
 
-Hint: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
 
-You will use localStorage to store any persistent data. For more information on how to work with the OpenWeather API, refer to the Full-Stack Blog on how to use API keys.
+## Installation
+
+You can reach this webpage through any web browser. No installation is required.
+https://azimezn.github.io/weather-dashboard/
+
+## Usage
+
+Type in a city name and a 5-day forecast will appear. It will store the last 5 searches and you can click on the city names in the search history to get the recent weather report again.
+
+![screenshot of coding quiz main page](assets/images/screenshot.png)
+
+## Credits
+
+- weather API from https://openweathermap.org/
+- Rutgers University Coding Bootcamp
+- [w3schools.com](W3schools.com)
+
+## License
+
+N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,145 +1,138 @@
+// global variables
 var $submitButton = document.querySelector("#submit-button");
 var $cityInput = document.querySelector("#city-input");
 var $cityName = document.querySelector(".city-name");
 var $currentDate = document.querySelector(".current-date");
 var $currentInfo = document.querySelector(".current-info");
-var $searchHistory = document.querySelector(".seach-history");
-var $dayTwo = document.querySelector("#day-two");
-var $dayThree = document.querySelector("#day-three");
-var $dayFour = document.querySelector("#day-four");
-var $dayFive = document.querySelector("#day-five");
-var $daySix = document.querySelector("#day-six");
+var $searchHistory = document.querySelector(".search-history");
+var $dayNumber = document.querySelector(".dayNumber");
 
-var city;
-
-$submitButton.addEventListener('click', weatherToday);
-
+// initial function
 function init() {
-
-    localStorage.getItem("city")
-    //$searchHistory.text = localStorage.getItem("city");
+    // goes to the function that gets from the local storage to the search history
+    loadFromLS();
 }
-init()
 
-
-
-function weatherToday() {
-
-    localStorage.setItem("city", $cityInput.value);
-    console.log($cityInput.value);
-    //$searchHistory.innerHTML = `<li>${$cityInput.value}</li>`
-
-    city = $cityInput.value;
-
+function weatherToday(city) {
+    // API to get current weather
     // "units=imperial" is to get results in fahrenheit
-    var weatherTodayAPI = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=48e33596e5a1232d3d02e65e8291a16d`
+    var weatherTodayAPI = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=48e33596e5a1232d3d02e65e8291a16d`;
 
-    fetch(weatherTodayAPI).then(response => {
-        if (response.ok) response.json()
-
-            .then(data => {
-
+    // checking the response and then fetching
+    fetch(weatherTodayAPI).then((response) => {
+        // if response.ok is true, then the API link is working
+        if (response.ok)
+            response.json().then((data) => {
+                // if the API link is working, but there is no data
                 if (data.length === 0) {
                     console.log("no link");
+                    // if there is data, then get these out of the data
                 } else {
+                    // function to save to local storage, taking the city name to it
+                    saveToLS(data.name);
+                    // display city name and current date
+                    $cityName.textContent =
+                        data.name + " " + dayjs().format("MM/DD/YYYY");
 
-                    $cityName.textContent = $cityInput.value + " " + (dayjs().format('MM/DD/YYYY'));
-
-                    $currentInfo.innerHTML =
-                    `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png"></img>
+                    //display weather information
+                    $currentInfo.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png"></img>
                     <li>Temperature: ${data.main.temp}</li>
                     <li>Humidity: ${data.main.humidity}</li>
                     <li>Wind Speed: ${data.wind.speed}</li>`;
-                    weatherFive();
-
+                    // continue to the 5-day forecast with the same city
+                    weatherFive(city);
                 }
-            })
-    })
-
+            });
+    });
 }
 
-function weatherFive() {
+function weatherFive(city) {
+    // API to get a 5 day weather forecast
+    var weatherFiveAPI = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&limit=1&units=imperial&appid=48e33596e5a1232d3d02e65e8291a16d`;
 
-    city = $cityInput.value;
-
-    var weatherFiveAPI = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&limit=1&units=imperial&appid=48e33596e5a1232d3d02e65e8291a16d`
-
-    fetch(weatherFiveAPI).then(response => {
-        if (response.ok) response.json()
-
-            .then(data => {
-                console.log(data)
-
+    fetch(weatherFiveAPI).then((response) => {
+        if (response.ok)
+            response.json().then((data) => {
                 if (data.length === 0) {
                     console.log("no link");
                 } else {
-
-                    $dayTwo.textContent = $cityInput.value + " " + (dayjs().add(1, 'day').format('MM/DD/YYYY'));
-                    $dayTwo.innerHTML +=
-                    `<img src="http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png"></img>
-                    <li>Temperature: ${data.list[0].main.temp}</li>
-                    <li>Humidity: ${data.list[0].main.humidity}</li>
-                    <li>Wind Speed: ${data.list[0].wind.speed}</li>`;
-
-                    $dayThree.textContent = $cityInput.value + " " + (dayjs().add(2, 'day').format('MM/DD/YYYY'));
-                    $dayThree.innerHTML +=
-                    `<img src="http://openweathermap.org/img/wn/${data.list[8].weather[0].icon}.png"></img>
-                    <li>Temperature: ${data.list[8].main.temp}</li>
-                    <li>Humidity: ${data.list[8].main.humidity}</li>
-                    <li>Wind Speed: ${data.list[8].wind.speed}</li>`;
-
-                    $dayFour.textContent = $cityInput.value + " " + (dayjs().add(3, 'day').format('MM/DD/YYYY'));
-                    $dayFour.innerHTML +=
-                    `<img src="http://openweathermap.org/img/wn/${data.list[16].weather[0].icon}.png"></img>
-                    <li>Temperature: ${data.list[16].main.temp}</li>
-                    <li>Humidity: ${data.list[16].main.humidity}</li>
-                    <li>Wind Speed: ${data.list[16].wind.speed}</li>`;
-
-                    $dayFive.textContent = $cityInput.value + " " + (dayjs().add(4, 'day').format('MM/DD/YYYY'));
-                    $dayFive.innerHTML +=
-                    `<img src="http://openweathermap.org/img/wn/${data.list[24].weather[0].icon}.png"></img>
-                    <li>Temperature: ${data.list[24].main.temp}</li>
-                    <li>Humidity: ${data.list[24].main.humidity}</li>
-                    <li>Wind Speed: ${data.list[24].wind.speed}</li>`;
-
-                    $daySix.textContent = $cityInput.value + " " + (dayjs().add(5, 'day').format('MM/DD/YYYY'));
-                    $daySix.innerHTML +=
-                    `<img src="http://openweathermap.org/img/wn/${data.list[32].weather[0].icon}.png"></img>
-                    <li>Temperature: ${data.list[32].main.temp}</li>
-                    <li>Humidity: ${data.list[32].main.humidity}</li>
-                    <li>Wind Speed: ${data.list[32].wind.speed}</li>`;
-
-
-                    
-
-
+                    console.log(data);
+                    // made a variable for the day becuase i increases by 8, but the day number should only increase by 1
+                    var day = 0;
+                    // clear previous weather info
+                    $dayNumber.innerHTML = "";
+                    // for loop to create a new list element and add weather information for the next 5-days
+                    for (var i = 0; i < 33; i += 8) {
+                        $dayNumber.innerHTML += `<section class="weather-box col">
+                        <h4>${dayjs().add(day++, "day").format("MM/DD/YYYY")}</h4>
+                        <img src="http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png"></img>
+                        <li>Temperature: ${data.list[i].main.temp}</li>
+                        <li>Humidity: ${data.list[i].main.humidity}</li>
+                        <li>Wind Speed: ${data.list[i].wind.speed}</li></section>`;
+                    }
                 }
-            })
-    })
-
-
+            });
+    });
 }
 
+// function to save to local storage
+function saveToLS(cityToSave) {
+    // if there is a city, get it. if not, keep it empty
+    var localArray = JSON.parse(localStorage.getItem("city")) || [];
+    // if city name is already in the array, don't add it
+    // cityToSave is the city name from the data
+    for (var i = 0; i < localArray.length; i++) {
+        if (localArray[i] === cityToSave) {
+            return;
+        }
+    }
 
+    // function to display the search history and take the city name to it
+    createHistory(cityToSave);
 
+    // add city to the beginning of the array of searched cities
+    localArray.unshift(cityToSave);
+    // if there is more than 5 cities, delete the last one
+    if (localArray.length > 5) {
+        localArray.pop();
+    }
 
-
-function displayWeather(data) {
-
-
-
-
-
-    
-
-
-
+    // add city to the array in the local storage
+    localStorage.setItem("city", JSON.stringify(localArray));
 }
 
+// get from local storage and display in search history
+function loadFromLS() {
+    var historyArray = JSON.parse(localStorage.getItem("city"));
 
-// function displayForcast ()
+    for (var i = 0; i < historyArray.length; i++) {
+        createHistory(historyArray[i]);
+    }
+}
 
+// display city in search history. its a separate function because I use it more than once
+function createHistory(name) {
+    $searchHistory.innerHTML += `<li class="history-tabs">${name}</li>`;
+}
 
+// make sure they're clicking specific cities in the search history
+function searchHistory(event) {
+    if (event.target.matches("li")) {
+        weatherToday(event.target.textContent);
+    }
+}
 
+// because im going through functions, I can't declare the variable in the weather function. i have to do it separately when adding a new one and using an old one
+function searchNewCity() {
+    var city = $cityInput.value;
+    // check the weather
+    weatherToday(city);
+}
 
-// function createPrevCityClick ()
+// submit button
+$submitButton.addEventListener("click", searchNewCity);
+// search history
+$searchHistory.addEventListener("click", searchHistory);
+
+// run initial function
+init();
